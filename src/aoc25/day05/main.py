3d5626fe9eb298472900e5cf1187be07f38467aa
Mike Franklin <mfranklin@sampleserve.com>
2025-12-05 07:52:19 -0500

    Day 5 step 2 done

--- src/aoc25/day05/main.py
+++ src/aoc25/day05/main.py
@@ -18,11 +18,13 @@ def load_file(path: Path) -> tuple[list[tuple[int, int]], list[int]]:
 
     return ranges, values
 
+
 def is_in_ranges(ranges: list[tuple[int, int]], value: int) -> bool:
     for start, end in ranges:
         if start <= value <= end:
             return True
     return False
+
 
 def step1():
     ranges, values = load_file(INPUT)
@@ -32,5 +34,25 @@ def step1():
             total_fresh += 1
     print(f"Example: {total_fresh} fresh values")
 
+
+def step2():
+    ranges, _ = load_file(INPUT)
+    sorted_ranges = sorted(ranges)
+    merged_ranges = []
+    current_range = sorted_ranges[0]
+    for start, end in sorted_ranges[1:]:
+        if start > current_range[1] + 1:
+            merged_ranges.append(current_range)
+            current_range = (start, end)
+        else:
+            current_range = (current_range[0], max(current_range[1], end))
+
+    merged_ranges.append(current_range)
+    print(
+        f"Merged Ranges: {sum(end - start + 1 for start, end in merged_ranges)} total values covered"
+    )
+
+
 if __name__ == "__main__":
     step1()
+    step2()
