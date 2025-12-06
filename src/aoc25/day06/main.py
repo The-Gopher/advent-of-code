e22503f4d196a0b8d2185872e81bdf72faa271aa
Mike Franklin <mfranklin@sampleserve.com>
2025-12-06 07:41:42 -0500

    Day 6 - step 2 - attempt 1

--- src/aoc25/day06/main.py
+++ src/aoc25/day06/main.py
@@ -1,3 +1,5 @@
+from curses import raw
+from itertools import zip_longest
 from pathlib import Path
 import re
 
@@ -20,8 +22,33 @@ def step1():
             for n in operands:
                 result *= n
             total += result
-    print(total)
+    print("Step 1:", total)
+
+
+def step2():
+    data: list[list[str]] = [
+        re.split(r" +", line.strip()) for line in INPUT.read_text().splitlines()
+    ]
+    total = 0
+    for line in zip(*data):
+        op = line[-1]
+        raw_numbers: list[str] = line[:-1]
+
+        operands = [
+            int("".join(n for n in nums if n is not None))
+            for nums in zip_longest(*(reversed(n) for n in raw_numbers))
+        ]
+        if op == "+":
+            total += sum(operands)
+        elif op == "*":
+            result = 1
+            for n in operands:
+                result *= n
+            total += result
+    print("Step 2:", total)
 
 
 if __name__ == "__main__":
     step1()
+    step2()
+    # Attempt 1 - 9302499534581 - too low
