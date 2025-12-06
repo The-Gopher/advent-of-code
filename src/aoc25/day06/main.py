7a8e3ca2616376ca47ab89e67e8c240d7232dacc
Mike Franklin <mfranklin@sampleserve.com>
2025-12-06 07:54:40 -0500

    Day 6 step 2 done

--- src/aoc25/day06/main.py
+++ src/aoc25/day06/main.py
@@ -26,25 +26,29 @@ def step1():
 
 
 def step2():
-    data: list[list[str]] = [
-        re.split(r" +", line.strip()) for line in INPUT.read_text().splitlines()
-    ]
+    data: list[str] = INPUT.read_text().splitlines(keepends=True)
     total = 0
-    for line in zip(*data):
-        op = line[-1]
-        raw_numbers: list[str] = line[:-1]
+    last_op = None
+    operands = []
+    for col in zip_longest(*data):
+        if set(col) == {" "} or set(col) == {"\n", None}:
+            if last_op == "+":
+                total += sum(operands)
+            elif last_op == "*":
+                result = 1
+                for n in operands:
+                    result *= n
+                total += result
+            last_op = None
+            operands = []
+            continue
 
-        operands = [
-            int("".join(n for n in nums if n is not None))
-            for nums in zip_longest(*(reversed(n) for n in raw_numbers))
-        ]
-        if op == "+":
-            total += sum(operands)
-        elif op == "*":
-            result = 1
-            for n in operands:
-                result *= n
-            total += result
+        if col[-1] in ("+", "*"):
+            last_op = col[-1]
+            operands = []
+
+        operands.append(int("".join(c for c in col[:-1] if c != " ")))
+
     print("Step 2:", total)
 
 
@@ -52,3 +56,10 @@ if __name__ == "__main__":
     step1()
     step2()
     # Attempt 1 - 9302499534581 - too low
+    # This Broke it
+    # 59   6 67
+    # 32  44 44
+    # 691 63 367
+    # 254 77 298
+    # +   +  +
+    # Attempt 2 - 9348430857627
