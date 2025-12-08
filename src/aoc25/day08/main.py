9aba91c7b6cdee2c51ba9d7efd9328bf6c66bf9a
Mike Franklin <mfranklin@sampleserve.com>
2025-12-08 09:26:00 -0500

    Day 8 part 2 done

--- src/aoc25/day08/main.py
+++ src/aoc25/day08/main.py
@@ -31,7 +31,7 @@ def step1():
 
     circuits: list[set[Point]] = []
 
-    for dist, p1, p2 in sorted_by_distance[:1000]:
+    for dist, p1, p2 in sorted_by_distance[:10]:
         assert p1 is not p2
 
         p1_circuit = set([p1])
@@ -55,9 +55,40 @@ def step1():
 
 
 def step2():
-    pass
+    points_from_file = [
+        Point(*map(int, line.strip().split(",")))
+        for line in INPUT.read_text().splitlines()
+    ]
+    pairwise_combinations = combinations(points_from_file, 2)
+    pairwise_with_distince = [
+        (distance_sq(p1, p2), p1, p2) for p1, p2 in pairwise_combinations
+    ]
+    sorted_by_distance = sorted(pairwise_with_distince, key=lambda x: x[0])
+
+    circuits: list[set[Point]] = []
+
+    for dist, p1, p2 in sorted_by_distance:
+        assert p1 is not p2
+
+        p1_circuit = set([p1])
+        p2_circuit = set([p2])
+        for circuit in circuits:
+            if p1 in circuit:
+                p1_circuit = circuit
+            if p2 in circuit:
+                p2_circuit = circuit
+
+        updated_circuits = [
+            c for c in circuits if c is not p1_circuit and c is not p2_circuit
+        ]
+        merged_circuit = p1_circuit.union(p2_circuit)
+        if len(merged_circuit) == len(points_from_file):
+            print("Step 2: ", p1.x * p2.x)
+            break
+        updated_circuits.append(merged_circuit)
+        circuits = updated_circuits
 
 
 if __name__ == "__main__":
-    step1()
+    # step1()
     step2()
